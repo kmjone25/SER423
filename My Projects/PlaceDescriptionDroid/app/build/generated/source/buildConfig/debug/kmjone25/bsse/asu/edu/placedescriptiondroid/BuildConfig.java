523f2e47da61003957d3a2356293d2c41c952e56
kmjone25 <kmjone25@asu.edu>
2017-03-21 23:00:35 -0400

    Organized

---
+++ My Projects/PlaceDescriptionDroid/app/build/generated/source/buildConfig/debug/kmjone25/bsse/asu/edu/placedescriptiondroid/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package kmjone25.bsse.asu.edu.placedescriptiondroid;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "kmjone25.bsse.asu.edu.placedescriptiondroid";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
